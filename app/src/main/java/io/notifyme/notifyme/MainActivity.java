631cec5b18e48c538257e57a98e742bcff0f78a6
Cesar Jr Rodriguez <cesarodriguez4@gmail.com>
2017-02-19 01:45:29 -0400

    Adding toast when the button is pressed

--- app/src/main/java/io/notifyme/notifyme/MainActivity.java
+++ app/src/main/java/io/notifyme/notifyme/MainActivity.java
@@ -2,6 +2,8 @@ package io.notifyme.notifyme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void send_message(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(), R.string.sms_send, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
